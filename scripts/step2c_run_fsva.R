93b58a45fe435d05b55db0c4e5819792f24b22a6
chenlab-sj <54188192+chenlab-sj@users.noreply.github.com>
2024-10-01 15:09:45 -0500

    Add files via upload

---
+++ scripts/step2c_run_fsva.R
@@ -0,0 +1,48 @@
+library(sva)
+library(limma)
+
+##########################################################i##########################################################
+#  Module load R/4.3.1-shlib                                                                                        #
+#                                                                                                                   #
+#  Commandline Example to run fSVA estimate:                                                                        #
+#  R CMD BATCH --no-save --no-restore '--args trainPhenoFile="pheno_file"  trainDataFile="QN_normalized_train_file" #
+#    model_file="SVA_model" testDataFile="QN_normalized_test_file" outprefix="solid_sjInternal"' run_fSVA.R fSVA.out#
+#                                                                                                                   #
+#####################################################################################################################
+
+args=(commandArgs(TRUE))
+
+if(length(args)==0){
+   print("No arguments supplied. Try again")
+   quit(save = "no", status = 1, runLast = TRUE)
+} else {
+    for(i in 1:length(args)){
+        eval(parse(text=args[[i]]))
+    }
+}
+
+trainPheno = read.table(trainPhenoFile, header=T)
+trainData  = read.table(trainDataFile, header=T)
+trainMatrix = data.matrix(trainData)
+
+testData = read.table(testDataFile, header=T)
+testMatrix = data.matrix(testData)
+
+trainMod  = model.matrix(~cancer, data=trainPheno)
+trainMod0 = model.matrix(~1,data=trainPheno)
+
+load(model_file)
+
+fsvaobj = fsva(trainMatrix, trainMod, trainSv, testMatrix, method = "fast")
+
+train_output = paste(outprefix, "_train_fsva_expression.txt", sep="")
+test_output  = paste(outprefix, "_test_fsva_expression.txt", sep="")
+
+write.table(fsvaobj$db, train_output, sep="\t", row.names=TRUE, quote=FALSE)
+write.table(fsvaobj$new, test_output, sep="\t", row.names=TRUE, quote=FALSE)
+
+# fsvaobj contains:
+# 1) db:  An adjusted version of the training database where the effect of batch/expression heterogeneity has been removed
+# 2) new: An adjusted version of the new samples, adjusted one at a time using the fsva methodology
+# 3) newsv: Surrogate variables for the new samples
+quit(save = "no", status = 0, runLast = TRUE)
