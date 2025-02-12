f0f4e1c12671ca7ac21fb0d847e2396ece8ae67f
chenlab-sj <54188192+chenlab-sj@users.noreply.github.com>
2025-02-12 15:39:01 -0600

    Update unpack_models.sh

--- unpack_models.sh
+++ unpack_models.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
   
-BASE_DIR=$PWD
-cd models
-tar -xvzf heme_pca85_CanID_model.tar.gz
-tar -xvzf heme_pca85_model.tar.gz
-rm heme_pca85_CanID_model.tar.gz
-rm heme_pca85_model.tar.gz
+##########################################
+# unpack the intermediate files
+##########################################
+mkdir -p ./intermediate_files/build
+
+# HM Expression File
+for file in ./intermediate_files/HM*.tar.gz; do
+    tar -xvzf "$file" -C ./intermediate_files/build
+done
+
+cat ./intermediate_files/build/HM_part_{00..17} > ./intermediate_files/build/HM_train_expression.txt
+rm ./intermediate_files/build/HM_part_??
+
+# ST Expression File
+for file in ./intermediate_files/ST*.tar.gz; do
+    tar -xvzf "$file" -C ./intermediate_files/build
+done
+
+cat ./intermediate_files/build/ST_part_{00..17} > ./intermediate_files/build/ST_train_expression.txt
+rm ./intermediate_files/build/ST_part_??
+
+# Place Final file in intermediate files directory
+mv ./intermediate_files/build/*.txt ./intermediate_files
+rmdir ./intermediate_files/build
+
+##########################################
+# unpack the HM pca model
+##########################################
+
+cat ./models/HM_pca85_part_{00..04} > ./models/HM_train_pca85.pickle
